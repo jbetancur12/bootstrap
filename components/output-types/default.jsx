1bac1c09422357da29235ff8cc3cc950938e49c4
Neal Malkani <Neal.Malkani@washpost.com>
2019-07-10 14:29:28 -0400

    fixing all lint errors

--- components/output-types/default.jsx
+++ components/output-types/default.jsx
@@ -1,28 +1,45 @@
-'use strict'
+import React from "react";
+import PropTypes from "prop-types";
 
-import React from 'react'
+const DefaultOutputType = props => {
+  const {
+    children,
+    contextPath,
+    deployment,
+    CssLinks,
+    Fusion,
+    Libs,
+    MetaTags
+  } = props;
+  return (
+    <html>
+      <head>
+        <title>Fusion Article</title>
+        <MetaTags />
+        <Libs />
+        <CssLinks />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href={deployment(`${contextPath}/resources/favicon.ico`)}
+        />
+      </head>
+      <body>
+        <div id="fusion-app">{children}</div>
+        <Fusion />
+      </body>
+    </html>
+  );
+};
 
-export default ({
-  children,
-  contextPath,
-  deployment,
-  CssLinks,
-  Fusion,
-  Libs,
-  MetaTags
-}) =>
-  <html>
-    <head>
-      <title>Fusion Article</title>
-      <MetaTags />
-      <Libs />
-      <CssLinks />
-      <link rel='icon' type='image/x-icon' href={deployment(`${contextPath}/resources/favicon.ico`)} />
-    </head>
-    <body>
-      <div id='fusion-app'>
-        {children}
-      </div>
-      <Fusion />
-    </body>
-  </html>
+DefaultOutputType.propTypes = {
+  children: PropTypes.node,
+  contextPath: PropTypes.string,
+  deployment: PropTypes.func,
+  CssLinks: PropTypes.object,
+  Fusion: PropTypes.func,
+  Libs: PropTypes.array,
+  MetaTags: PropTypes.object
+};
+
+export default DefaultOutputType;
